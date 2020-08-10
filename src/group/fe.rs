ac42987b7c9cdd163232d1dce9abba6a18399639
ross <ross.pure@gmail.com>
2020-08-10 16:42:50 +1000

    initial curve point API

--- src/group/fe.rs
+++ src/group/fe.rs
@@ -1,6 +1,7 @@
 use libc::{c_int, c_uchar, size_t};
 
 #[repr(C)]
+#[derive(Copy, Clone, Default)]
 pub struct Fe {
     limbs: [u64; 5],
 }
@@ -13,27 +14,27 @@ extern "C" {
     fn secp256k1_fe_normalizes_to_zero_var_export(r: *mut Fe) -> c_int;
     fn secp256k1_fe_set_int_export(r: *mut Fe, a: c_int);
     fn secp256k1_fe_clear_export(a: *mut Fe);
-    fn secp256k1_fe_is_zero_export(a: *mut Fe) -> c_int;
-    fn secp256k1_fe_is_odd_export(a: *mut Fe) -> c_int;
-    fn secp256k1_fe_equal_export(a: *mut Fe, b: *mut Fe) -> c_int;
-    fn secp256k1_fe_equal_var_export(a: *mut Fe, b: *mut Fe) -> c_int;
-    fn secp256k1_fe_cmp_var_export(a: *mut Fe, b: *mut Fe) -> c_int;
-    fn secp256k1_fe_set_b32_export(r: *mut Fe, a: *mut c_uchar) -> c_int;
-    fn secp256k1_fe_get_b32_export(r: *mut c_uchar, a: *mut Fe);
-    fn secp256k1_fe_negate_export(r: *mut Fe, a: *mut Fe, m: c_int);
+    fn secp256k1_fe_is_zero_export(a: *const Fe) -> c_int;
+    fn secp256k1_fe_is_odd_export(a: *const Fe) -> c_int;
+    fn secp256k1_fe_equal_export(a: *const Fe, b: *const Fe) -> c_int;
+    fn secp256k1_fe_equal_var_export(a: *const Fe, b: *const Fe) -> c_int;
+    fn secp256k1_fe_cmp_var_export(a: *const Fe, b: *const Fe) -> c_int;
+    fn secp256k1_fe_set_b32_export(r: *mut Fe, a: *const c_uchar) -> c_int;
+    fn secp256k1_fe_get_b32_export(r: *mut c_uchar, a: *const Fe);
+    fn secp256k1_fe_negate_export(r: *mut Fe, a: *const Fe, m: c_int);
     fn secp256k1_fe_mul_int_export(r: *mut Fe, a: c_int);
-    fn secp256k1_fe_add_export(r: *mut Fe, a: *mut Fe);
-    fn secp256k1_fe_mul_export(r: *mut Fe, a: *mut Fe, b: *const Fe);
-    fn secp256k1_fe_sqr_export(r: *mut Fe, a: *mut Fe);
-    fn secp256k1_fe_sqrt_export(r: *mut Fe, a: *mut Fe) -> c_int;
-    fn secp256k1_fe_is_quad_var_export(a: *mut Fe) -> c_int;
-    fn secp256k1_fe_inv_export(r: *mut Fe, a: *mut Fe);
-    fn secp256k1_fe_inv_var_export(r: *mut Fe, a: *mut Fe);
-    fn secp256k1_fe_inv_all_var_export(r: *mut Fe, a: *mut Fe, len: size_t);
+    fn secp256k1_fe_add_export(r: *mut Fe, a: *const Fe);
+    fn secp256k1_fe_mul_export(r: *mut Fe, a: *const Fe, b: *const Fe);
+    fn secp256k1_fe_sqr_export(r: *mut Fe, a: *const Fe);
+    fn secp256k1_fe_sqrt_export(r: *mut Fe, a: *const Fe) -> c_int;
+    fn secp256k1_fe_is_quad_var_export(a: *const Fe) -> c_int;
+    fn secp256k1_fe_inv_export(r: *mut Fe, a: *const Fe);
+    fn secp256k1_fe_inv_var_export(r: *mut Fe, a: *const Fe);
+    fn secp256k1_fe_inv_all_var_export(r: *mut Fe, a: *const Fe, len: size_t);
     // fn secp256k1_fe_to_storage_export(secp256k1_fe_storage *r, a: *mut Fe);
     // fn secp256k1_fe_from_storage_export(r: *mut Fe, const secp256k1_fe_storage *a);
     // fn secp256k1_fe_storage_cmov_export(secp256k1_fe_storage *r, const secp256k1_fe_storage *a, flag: c_int);
-    fn secp256k1_fe_cmov_export(r: *mut Fe, a: *mut Fe, flag: c_int);
+    fn secp256k1_fe_cmov_export(r: *mut Fe, a: *const Fe, flag: c_int);
 }
 
 pub fn normalize(r: &mut Fe) {
@@ -57,57 +58,142 @@ pub fn set_int(r: &mut Fe, a: c_int) {
 pub fn clear(a: &mut Fe) {
     unsafe { secp256k1_fe_clear_export(a) }
 }
-pub fn is_zero(a: &mut Fe) -> c_int {
+pub fn is_zero(a: &Fe) -> c_int {
     unsafe { secp256k1_fe_is_zero_export(a) }
 }
-pub fn is_odd(a: &mut Fe) -> c_int {
+pub fn is_odd(a: &Fe) -> c_int {
     unsafe { secp256k1_fe_is_odd_export(a) }
 }
-pub fn equal(a: &mut Fe, b: &mut Fe) -> c_int {
+pub fn equal(a: &Fe, b: &Fe) -> c_int {
     unsafe { secp256k1_fe_equal_export(a, b) }
 }
-pub fn equal_var(a: &mut Fe, b: &mut Fe) -> c_int {
+pub fn equal_var(a: &Fe, b: &Fe) -> c_int {
     unsafe { secp256k1_fe_equal_var_export(a, b) }
 }
-pub fn cmp_var(a: &mut Fe, b: &mut Fe) -> c_int {
+pub fn cmp_var(a: &Fe, b: &Fe) -> c_int {
     unsafe { secp256k1_fe_cmp_var_export(a, b) }
 }
-pub fn set_b32(r: &mut Fe, a: &mut c_uchar) -> c_int {
+pub fn set_b32(r: &mut Fe, a: &c_uchar) -> c_int {
     unsafe { secp256k1_fe_set_b32_export(r, a) }
 }
-pub fn get_b32(r: &mut c_uchar, a: &mut Fe) {
+pub fn get_b32(r: &mut c_uchar, a: &Fe) {
     unsafe { secp256k1_fe_get_b32_export(r, a) }
 }
-pub fn negate(r: &mut Fe, a: &mut Fe, m: c_int) {
+pub fn negate(r: &mut Fe, a: &Fe, m: c_int) {
     unsafe { secp256k1_fe_negate_export(r, a, m) }
 }
 pub fn mul_int(r: &mut Fe, a: c_int) {
     unsafe { secp256k1_fe_mul_int_export(r, a) }
 }
-pub fn add(r: &mut Fe, a: &mut Fe) {
+pub fn add(r: &mut Fe, a: &Fe) {
     unsafe { secp256k1_fe_add_export(r, a) }
 }
-pub fn mul(r: &mut Fe, a: &mut Fe, b: &Fe) {
+pub fn mul(r: &mut Fe, a: &Fe, b: &Fe) {
     unsafe { secp256k1_fe_mul_export(r, a, b) }
 }
-pub fn sqr(r: &mut Fe, a: &mut Fe) {
+pub fn sqr(r: &mut Fe, a: &Fe) {
     unsafe { secp256k1_fe_sqr_export(r, a) }
 }
-pub fn sqrt(r: &mut Fe, a: &mut Fe) -> c_int {
+pub fn sqrt(r: &mut Fe, a: &Fe) -> c_int {
     unsafe { secp256k1_fe_sqrt_export(r, a) }
 }
-pub fn is_quad_var(a: &mut Fe) -> c_int {
+pub fn is_quad_var(a: &Fe) -> c_int {
     unsafe { secp256k1_fe_is_quad_var_export(a) }
 }
-pub fn inv(r: &mut Fe, a: &mut Fe) {
+pub fn inv(r: &mut Fe, a: &Fe) {
     unsafe { secp256k1_fe_inv_export(r, a) }
 }
-pub fn inv_var(r: &mut Fe, a: &mut Fe) {
+pub fn inv_var(r: &mut Fe, a: &Fe) {
     unsafe { secp256k1_fe_inv_var_export(r, a) }
 }
-pub fn inv_all_var(r: &mut Fe, a: &mut Fe, len: size_t) {
+pub fn inv_all_var(r: &mut Fe, a: &Fe, len: size_t) {
     unsafe { secp256k1_fe_inv_all_var_export(r, a, len) }
 }
-pub fn cmov(r: &mut Fe, a: &mut Fe, flag: c_int) {
+pub fn cmov(r: &mut Fe, a: &Fe, flag: c_int) {
     unsafe { secp256k1_fe_cmov_export(r, a, flag) }
+}
+
+impl Fe {
+    pub fn mul(&mut self, a: &Fe, b: &Fe) {
+        unsafe { secp256k1_fe_mul_export(self, a, b) }
+    }
+
+    pub fn mul_assign(&mut self, a: &Fe) {
+        unsafe { secp256k1_fe_mul_export(self, self, a) }
+    }
+
+    pub fn inv(&mut self, a: &Fe) {
+        unsafe { secp256k1_fe_inv_var_export(self, a) }
+    }
+
+    pub fn normalize(&mut self) {
+        unsafe { secp256k1_fe_normalize_var_export(self) }
+    }
+}
+
+impl PartialEq for Fe {
+    fn eq(&self, other: &Self) -> bool {
+        let mut tmp_fe = *self;
+        let mut tmp_other_fe = *other;
+        tmp_fe.normalize();
+        tmp_other_fe.normalize();
+        tmp_fe.limbs == tmp_other_fe.limbs
+    }
+}
+impl Eq for Fe {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_fe_add(b: &mut Bencher) {
+        let mut r = Fe {
+            limbs: [123, 234, 345, 456, 567],
+        };
+        let mut x = Fe {
+            limbs: [987, 876, 765, 654, 543],
+        };
+        b.iter(|| add(&mut r, &mut x));
+    }
+
+    #[bench]
+    fn bench_fe_mul(b: &mut Bencher) {
+        let mut r = Fe { limbs: [0; 5] };
+        let mut x = Fe {
+            limbs: [123, 234, 345, 456, 567],
+        };
+        let mut y = Fe {
+            limbs: [987, 876, 765, 654, 543],
+        };
+        b.iter(|| mul(&mut r, &mut x, &mut y));
+    }
+
+    #[bench]
+    fn bench_fe_inverse(b: &mut Bencher) {
+        let mut r = Fe { limbs: [0; 5] };
+        let mut x = Fe {
+            limbs: [123, 234, 345, 456, 567],
+        };
+        b.iter(|| inv_var(&mut r, &mut x));
+    }
+
+    #[bench]
+    fn bench_fe_normalize(b: &mut Bencher) {
+        b.iter(|| {
+            // r is set an defined in here to try to avoid later iterations working on already
+            // normalized data.
+            let mut r = Fe {
+                limbs: [
+                    0xfefe_fefe_fefe_fefe,
+                    0xdcdc_dcdc_dcdc_dcdc,
+                    0xbaba_baba_baba_baba,
+                    0x9898_9898_9898_9898,
+                    0x7676_7676_7676_7676,
+                ],
+            };
+            normalize_var(&mut r)
+        });
+    }
 }
--- src/group/mod.rs
+++ src/group/mod.rs
@@ -1,4 +1,4 @@
-use libc::{c_int, size_t};
+use libc::c_int;
 
 pub mod fe;
 
@@ -6,6 +6,7 @@ use crate::scalar::Scalar;
 use fe::Fe;
 
 #[repr(C)]
+#[derive(Copy, Clone, Default)]
 pub struct Ge {
     x: Fe,
     y: Fe,
@@ -13,6 +14,7 @@ pub struct Ge {
 }
 
 #[repr(C)]
+#[derive(Copy, Clone, Default)]
 pub struct Gej {
     x: Fe,
     y: Fe,
@@ -21,6 +23,8 @@ pub struct Gej {
 }
 
 extern "C" {
+    static secp256k1_ge_const_g: Ge;
+
     fn secp256k1_ge_set_xy_export(r: *mut Ge, x: *const Fe, y: *const Fe);
     fn secp256k1_ge_set_xquad_export(r: *mut Ge, x: *const Fe) -> c_int;
     fn secp256k1_ge_set_xo_var_export(r: *mut Ge, x: *const Fe, odd: c_int) -> c_int;
@@ -28,14 +32,16 @@ extern "C" {
     fn secp256k1_ge_is_valid_var_export(a: *const Ge) -> c_int;
     fn secp256k1_ge_neg_export(r: *mut Ge, a: *const Ge);
     fn secp256k1_ge_set_gej_export(r: *mut Ge, a: *mut Gej);
-    fn secp256k1_ge_set_all_gej_var_export(r: *mut Ge, a: *const Gej, len: size_t);
-    fn secp256k1_ge_globalz_set_table_gej_export(
-        len: size_t,
-        r: *mut Ge,
-        globalz: *mut Fe,
-        a: *const Gej,
-        zr: *const Fe,
-    );
+    // fn secp256k1_ge_set_all_gej_var_export(r: *mut Ge, a: *const Gej, len: size_t);
+    /*
+        fn secp256k1_ge_globalz_set_table_gej_export(
+            len: size_t,
+            r: *mut Ge,
+            globalz: *mut Fe,
+            a: *const Gej,
+            zr: *const Fe,
+        );
+    */
     fn secp256k1_ge_set_infinity_export(r: *mut Ge);
     fn secp256k1_gej_set_infinity_export(r: *mut Gej);
     fn secp256k1_gej_set_ge_export(r: *mut Gej, a: *const Ge);
@@ -43,17 +49,19 @@ extern "C" {
     fn secp256k1_gej_neg_export(r: *mut Gej, a: *const Gej);
     fn secp256k1_gej_is_infinity_export(a: *const Gej) -> c_int;
     fn secp256k1_gej_has_quad_y_var_export(a: *const Gej) -> c_int;
-    fn secp256k1_gej_double_export(r: *mut Gej, a: *const Gej);
+    // fn secp256k1_gej_double_export(r: *mut Gej, a: *const Gej);
     fn secp256k1_gej_double_var_export(r: *mut Gej, a: *const Gej, rzr: *mut Fe);
     fn secp256k1_gej_add_var_export(r: *mut Gej, a: *const Gej, b: *const Gej, rzr: *mut Fe);
-    fn secp256k1_gej_add_ge_export(r: *mut Gej, a: *const Gej, b: *const Ge);
+    // fn secp256k1_gej_add_ge_export(r: *mut Gej, a: *const Gej, b: *const Ge);
     fn secp256k1_gej_add_ge_var_export(r: *mut Gej, a: *const Gej, b: *const Ge, rzr: *mut Fe);
-    fn secp256k1_gej_add_zinv_var_export(
-        r: *mut Gej,
-        a: *const Gej,
-        b: *const Ge,
-        bzinv: *const Fe,
-    );
+    /*
+        fn secp256k1_gej_add_zinv_var_export(
+            r: *mut Gej,
+            a: *const Gej,
+            b: *const Ge,
+            bzinv: *const Fe,
+        );
+    */
     // fn secp256k1_ge_mul_lambda_export(*mut Ge r, *const Ge a);
     fn secp256k1_gej_clear_export(r: *mut Gej);
     fn secp256k1_ge_clear_export(r: *mut Ge);
@@ -64,3 +72,136 @@ extern "C" {
 
     fn secp256k1_ecmult_const_export(r: *mut Gej, a: *const Ge, q: *const Scalar, bits: c_int);
 }
+
+impl Ge {
+    pub fn set_xy(&mut self, x: &Fe, y: &Fe) {
+        unsafe { secp256k1_ge_set_xy_export(self, x, y) }
+    }
+
+    pub fn set_xquad(&mut self, x: &Fe) -> bool {
+        unsafe { secp256k1_ge_set_xquad_export(self, x) != 0 }
+    }
+
+    pub fn set_xo_var(&mut self, x: &Fe, odd: bool) -> bool {
+        let odd_cint = if odd { 1 } else { 0 };
+        unsafe { secp256k1_ge_set_xo_var_export(self, x, odd_cint) != 0 }
+    }
+    pub fn is_infinity(&self) -> bool {
+        unsafe { secp256k1_ge_is_infinity_export(self) != 0 }
+    }
+
+    pub fn is_valid_var(&self) -> bool {
+        unsafe { secp256k1_ge_is_valid_var_export(self) != 0 }
+    }
+
+    pub fn neg(&mut self, a: &Ge) {
+        unsafe { secp256k1_ge_neg_export(self, a) }
+    }
+
+    pub fn set_gej(&mut self, a: &mut Gej) {
+        unsafe { secp256k1_ge_set_gej_export(self, a) }
+    }
+
+    pub fn set_infinity(&mut self) {
+        unsafe { secp256k1_ge_set_infinity_export(self) }
+    }
+
+    pub fn clear(&mut self) {
+        unsafe { secp256k1_ge_clear_export(self) }
+    }
+
+    pub fn scalar_mul(&mut self, a: &Ge, q: &Scalar) {
+        let mut tmp_gej = Gej::default();
+        let bits = 257;
+        unsafe {
+            secp256k1_ecmult_const_export(&mut tmp_gej, a, q, bits);
+            secp256k1_ge_set_gej_export(self, &mut tmp_gej);
+        }
+    }
+
+    pub fn scalar_base_mul(&mut self, q: &Scalar) {
+        let mut tmp_gej = Gej::default();
+        let bits = 257;
+        unsafe {
+            secp256k1_ecmult_const_export(&mut tmp_gej, &secp256k1_ge_const_g, q, bits);
+            secp256k1_ge_set_gej_export(self, &mut tmp_gej);
+        }
+    }
+}
+
+impl Gej {
+    pub fn set_infinity(&mut self) {
+        unsafe { secp256k1_gej_set_infinity_export(self) }
+    }
+
+    pub fn set_ge(&mut self, a: &Ge) {
+        unsafe { secp256k1_gej_set_ge_export(self, a) }
+    }
+
+    pub fn eq_x(x: &Fe, a: &Gej) -> bool {
+        unsafe { secp256k1_gej_eq_x_var_export(x, a) != 0 }
+    }
+
+    pub fn neg(&mut self, a: &Gej) {
+        unsafe { secp256k1_gej_neg_export(self, a) }
+    }
+
+    pub fn is_infinity(a: &Gej) -> bool {
+        unsafe { secp256k1_gej_is_infinity_export(a) != 0 }
+    }
+
+    pub fn has_quad_y(a: &Gej) -> bool {
+        unsafe { secp256k1_gej_has_quad_y_var_export(a) != 0 }
+    }
+
+    pub fn double(&mut self, a: &Gej, rzr: &mut Fe) {
+        // NOTE: We are using the time variant function here because we are not conerned with a
+        // timining side channel, and it is faster.
+        unsafe { secp256k1_gej_double_var_export(self, a, rzr) }
+    }
+
+    pub fn add(&mut self, a: &Gej, b: &Gej, rzr: &mut Fe) {
+        unsafe { secp256k1_gej_add_var_export(self, a, b, rzr) }
+    }
+
+    pub fn add_ge_var(&mut self, a: &Gej, b: &Ge, rzr: &mut Fe) {
+        // NOTE: We are using the time variant function here because we are not conerned with a
+        // timining side channel, and it is faster.
+        unsafe { secp256k1_gej_add_ge_var_export(self, a, b, rzr) }
+    }
+
+    pub fn clear(&mut self) {
+        unsafe { secp256k1_gej_clear_export(self) }
+    }
+
+    pub fn rescale(&mut self, b: &Fe) {
+        unsafe { secp256k1_gej_rescale_export(self, b) }
+    }
+
+    pub fn scalar_mul(&mut self, a: &Ge, q: &Scalar) {
+        let bits = 257;
+        unsafe { secp256k1_ecmult_const_export(self, a, q, bits) }
+    }
+
+    pub fn scalar_base_mul(&mut self, q: &Scalar) {
+        let bits = 257;
+        unsafe { secp256k1_ecmult_const_export(self, &secp256k1_ge_const_g, q, bits) }
+    }
+}
+
+impl PartialEq for Gej {
+    fn eq(&self, other: &Self) -> bool {
+        let mut scale = Fe::default();
+        let mut tmp_gej = *self;
+        let mut tmp_other_gej = *other;
+        scale.inv(&self.z);
+        scale.mul_assign(&other.z);
+        tmp_gej.rescale(&scale);
+        tmp_gej.x.normalize();
+        tmp_gej.y.normalize();
+        tmp_other_gej.x.normalize();
+        tmp_other_gej.y.normalize();
+        (tmp_gej.x == tmp_other_gej.x) && (tmp_gej.y == tmp_other_gej.y)
+    }
+}
+impl Eq for Gej {}
